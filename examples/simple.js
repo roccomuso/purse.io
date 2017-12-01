752e18e572cf10c3ff51acffbf836e49e18ea8d6
roccomuso <rocco@hackerstribe.com>
2017-12-01 19:24:44 +0100

    renamed

---
+++ examples/simple.js
@@ -0,0 +1,20 @@
+let Purse = require('../index')
+
+let purse = new Purse({limit: 25})
+
+// get Offers
+
+purse.limit(25)
+  .offset(0)
+  .country('IT')
+  .amount(null)
+  .hide(null)
+  .fetch()
+    .then(console.log)
+    .catch(console.error)
+
+// get Rates
+
+purse.fetchRates()
+  .then(console.log)
+  .catch(console.error)
